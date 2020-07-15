1c6dd1c7181ba705b7ebd82ed592bc506165635d
Lucas Donato <lucas.donato@mblabs.com.br>
2020-07-15 18:28:36 -0300

    Initial commit

---
+++ app/src/main/java/br/com/stickers/splash/view/SplashActivity.kt
@@ -0,0 +1,18 @@
+package br.com.stickers.splash.view
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import br.com.stickers.EntryActivity
+import br.com.stickers.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+            startActivity(EntryActivity.getStartIntent(this))
+        }, 3000.toLong())
+    }
+}
