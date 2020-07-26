5afe3d5289dee9a2f22b7ed2a8a0a398d912f915
Lucas Donato <lucas.donato@mblabs.com.br>
2020-07-26 11:02:10 -0300

    change package name

---
+++ app/src/main/java/br/com/stickersbrasil/logging/FabricTree.kt
@@ -0,0 +1,21 @@
+package br.com.stickersbrasil.logging
+
+import com.google.firebase.crashlytics.FirebaseCrashlytics
+import timber.log.Timber
+
+class FabricTree : Timber.Tree() {
+
+    override fun log(
+            priority: Int,
+            tag: String?,
+            message: String,
+            t: Throwable?
+    ) {
+        val firebaseCrashlytics = FirebaseCrashlytics.getInstance()
+        if (t != null) {
+            firebaseCrashlytics.recordException(t)
+        } else {
+            firebaseCrashlytics.log("E/$tag: $message")
+        }
+    }
+}
---
+++ app/src/main/java/br/com/stickersbrasil/presentation/home/ViewController/StickerPackListItemViewHolder.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) WhatsApp Inc. and its affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+package br.com.stickersbrasil.presentation.home.ViewController
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import br.com.stickersbrasil.R
+
+internal class StickerPackListItemViewHolder(val container: View) : RecyclerView.ViewHolder(container) {
+    @JvmField
+    val titleView: TextView = container.findViewById(R.id.sticker_pack_title)
+
+    @JvmField
+    val publisherView: TextView = container.findViewById(R.id.sticker_pack_publisher)
+
+    @JvmField
+    val fileSizeView: TextView = container.findViewById(R.id.sticker_pack_filesize)
+
+    @JvmField
+    val addButton: ImageView = container.findViewById(R.id.add_button_on_list)
+
+    @JvmField
+    val imageRowView: LinearLayout = container.findViewById(R.id.sticker_packs_list_item_image_list)
+
+}
